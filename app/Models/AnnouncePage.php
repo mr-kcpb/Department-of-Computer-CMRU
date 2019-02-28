7e4f31084528095c5fb096b10fb867385c00e5ed
Gr4vity4 <prawared.bw@gmail.com>
2019-02-28 18:11:28 +0700

    add nova resource announce

--- app/Models/AnnouncePage.php
+++ app/Models/AnnouncePage.php
@@ -3,8 +3,17 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Auth;
 
 class AnnouncePage extends Model
 {
-    //
+    
+    protected $table = 'announce_pages';
+    protected $guarded = [];
+    protected $dates = ['deleted_at'];
+    
+    public function user()
+    {
+        return $this->belongsTo('App\Models\User', 'user_id');
+    }
 }
---
+++ app/Nova/Announce.php
@@ -0,0 +1,123 @@
+<?php
+
+namespace App\Nova;
+
+use Froala\NovaFroalaField\Froala;
+use Illuminate\Support\Facades\Auth;
+use Laravel\Nova\Fields\BelongsTo;
+use Laravel\Nova\Fields\ID;
+use Illuminate\Http\Request;
+use Laravel\Nova\Fields\Select;
+use Laravel\Nova\Fields\Text;
+use Laravel\Nova\Http\Requests\NovaRequest;
+
+class Announce extends Resource
+{
+    
+    /**
+     * The model the resource corresponds to.
+     *
+     * @var string
+     */
+    public static $model = 'App\Models\AnnouncePage';
+    
+    /**
+     * The single value that should be used to represent the resource when
+     * being displayed.
+     *
+     * @var string
+     */
+    public static $title = 'title';
+    
+    /**
+     * The columns that should be searched.
+     *
+     * @var array
+     */
+    public static $search
+        = [
+            'id',
+            'title',
+        ];
+    
+    /**
+     * Get the fields displayed by the resource.
+     *
+     * @param  \Illuminate\Http\Request $request
+     *
+     * @return array
+     */
+    public function fields(Request $request)
+    {
+        return [
+            ID::make()->sortable(),
+            
+            Text::make('Create by User ID', 'user_id')
+                ->withMeta(['value' => Auth::user()->id])
+                ->withMeta([
+                    'extraAttributes' => [
+                        'readonly' => true,
+                    ],
+                ]),
+            
+            Text::make('Title')
+                ->sortable()
+                ->rules('required', 'max:255'),
+            
+            Froala::make('Detail')
+                  ->rules('required')
+                  ->hideFromIndex()
+                  ->options([
+                      'height' => 600,
+                  ])->withFiles('public'),
+        ];
+    }
+    
+    /**
+     * Get the cards available for the request.
+     *
+     * @param  \Illuminate\Http\Request $request
+     *
+     * @return array
+     */
+    public function cards(Request $request)
+    {
+        return [];
+    }
+    
+    /**
+     * Get the filters available for the resource.
+     *
+     * @param  \Illuminate\Http\Request $request
+     *
+     * @return array
+     */
+    public function filters(Request $request)
+    {
+        return [];
+    }
+    
+    /**
+     * Get the lenses available for the resource.
+     *
+     * @param  \Illuminate\Http\Request $request
+     *
+     * @return array
+     */
+    public function lenses(Request $request)
+    {
+        return [];
+    }
+    
+    /**
+     * Get the actions available for the resource.
+     *
+     * @param  \Illuminate\Http\Request $request
+     *
+     * @return array
+     */
+    public function actions(Request $request)
+    {
+        return [];
+    }
+}
