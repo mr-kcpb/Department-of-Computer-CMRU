0ca70257ec12bb4c72cd478b828fbbdd98e4bf19
Gr4vity4 <prawared.bw@gmail.com>
2019-03-04 10:28:37 +0700

    Home page update

--- resources/views/front-end/home.blade.php
+++ resources/views/front-end/home.blade.php
@@ -134,10 +134,7 @@
                 </div>
                 <div class="col-xs-12 col-md-4">
                     <div class="form-group">
-                        <iframe
-                            src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FCMRUCOMPUTER%2F&tabs=timeline&width=340&height=215&small_header=false&adapt_container_width=false&hide_cover=false&show_facepile=true&appId=319916248660520"
-                            width="340" height="215" style="border:none;overflow:hidden" scrolling="no" frameborder="0"
-                            allowTransparency="true" allow="encrypted-media"></iframe>
+                        <iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FCMRUCOMPUTER%2F&tabs=timeline&width=340&height=500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=false&appId" width="340" height="500" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media"></iframe>
                     </div>
                 </div>
             </div>
