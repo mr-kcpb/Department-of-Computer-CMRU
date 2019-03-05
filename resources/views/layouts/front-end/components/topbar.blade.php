7c9cbe98cceaae6e7c1a858be999f684522fcbba
Gr4vity4 <prawared.bw@gmail.com>
2019-03-05 14:09:58 +0700

    Navbar logo update

--- resources/views/layouts/front-end/components/topbar.blade.php
+++ resources/views/layouts/front-end/components/topbar.blade.php
@@ -1,38 +1,38 @@
 <!-- Top bar starts -->
 {{--<div class="top-bar">--}}
-    <div class="container">
+<div class="container">
 
-<div class="row">
-    <div class="col-xs-12">
-        <div style="padding-top: 30px">
-            <a href="{{ route('home') }}">
-                {{--<img src="http://www.computer.cmru.ac.th/images/logo.png" alt="" class="img-responsive">--}}
-                <img src="https://www.freelogodesign.org/Content/img/logo-ex-7.png" alt="" class="" style="height: 100px">
-            </a>
+    <div class="row">
+        <div class="col-xs-12">
+            <div style="padding-top: 30px">
+                <a href="{{ route('home') }}">
+                    {{--<img src="http://www.computer.cmru.ac.th/images/logo.png" alt="" class="img-responsive">--}}
+                    <img src="{{ asset('images/computer-cmru.png') }}" alt="" class="img-responsive">
+                </a>
+            </div>
         </div>
     </div>
+{{--ภาควิชาคอมพิวเตอร์ คณะวิทยาศาสตร์และเทคโนโลยี มหาวิทยาลัยราชภัฏเชียงใหม่--}}
+
+
+<!-- Search section for responsive design -->
+{{--<div class="tb-search pull-left">--}}
+{{--<a href="#" class="b-dropdown"><i class="fa fa-search square-2 rounded-1 bg-color white"></i></a>--}}
+{{--<div class="b-dropdown-block">--}}
+{{--<form role="form">--}}
+{{--<!-- Input Group -->--}}
+{{--<div class="input-group">--}}
+{{--<input type="text" class="form-control" placeholder="Type Something">--}}
+{{--<span class="input-group-btn">--}}
+{{--<button class="btn btn-color" type="button">Search</button>--}}
+{{--</span>--}}
+{{--</div>--}}
+{{--</form>--}}
+{{--</div>--}}
+{{--</div>--}}
+<!-- Search section ends -->
+    {{--<div class="clearfix"></div>--}}
 </div>
-            {{--ภาควิชาคอมพิวเตอร์ คณะวิทยาศาสตร์และเทคโนโลยี มหาวิทยาลัยราชภัฏเชียงใหม่--}}
-
-
-        <!-- Search section for responsive design -->
-        {{--<div class="tb-search pull-left">--}}
-            {{--<a href="#" class="b-dropdown"><i class="fa fa-search square-2 rounded-1 bg-color white"></i></a>--}}
-            {{--<div class="b-dropdown-block">--}}
-                {{--<form role="form">--}}
-                    {{--<!-- Input Group -->--}}
-                    {{--<div class="input-group">--}}
-                        {{--<input type="text" class="form-control" placeholder="Type Something">--}}
-                        {{--<span class="input-group-btn">--}}
-										{{--<button class="btn btn-color" type="button">Search</button>--}}
-									{{--</span>--}}
-                    {{--</div>--}}
-                {{--</form>--}}
-            {{--</div>--}}
-        {{--</div>--}}
-        <!-- Search section ends -->
-        {{--<div class="clearfix"></div>--}}
-    </div>
 {{--</div>--}}
 
 <!-- Top bar ends -->
